fd7e80a16b309c5684782321255a9a9023306aa8
sonu1927thakur@gmail.com <sonu-github923>
2025-09-02 21:33:08 +0530

    @sonu

--- grpcDemo/src/main/java/com/grpc/grpcDemo/GrpcDemoApplication.java
+++ grpcDemo/src/main/java/com/grpc/grpcDemo/GrpcDemoApplication.java
@@ -1,5 +1,7 @@
 package com.grpc.grpcDemo;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,7 @@ public class GrpcDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GrpcDemoApplication.class, args);
+		System.out.println("-------------Default JVM TimeZone--------------: " + TimeZone.getDefault().getID());
 	}
 
 }
