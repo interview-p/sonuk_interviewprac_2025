f9a63b8fb4bf1699379b1623f33855c541163fc3
sonu1927thakur@gmail.com <sonu-github923>
2025-12-30 20:53:09 +0530

    @sonu

---
+++ kafkaDebeziumConsumer/src/main/java/com/kafka/debeziumConsumer/kafkaDebeziumConsumer/Listener/DebeziumConsumer.java
@@ -0,0 +1,21 @@
+package com.kafka.debeziumConsumer.kafkaDebeziumConsumer.Listener;
+
+import org.apache.avro.generic.GenericRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DebeziumConsumer {
+
+	@KafkaListener(topics = "appdb.public.users", groupId = "appdb-consumer")
+	public void listen(ConsumerRecord<String, GenericRecord> record,Acknowledgment ack) {
+
+       GenericRecord value = record.value();
+
+       System.out.println("Received event: " + value);
+
+       ack.acknowledge();
+}
+}
